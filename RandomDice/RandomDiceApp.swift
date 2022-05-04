d337e237129aee67f9a0cad33e0a5edb16717277
jojo <jojo22123770@gmail.com>
2022-05-04 11:03:07 +0800

    Initial Commit

---
+++ RandomDice/RandomDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDiceApp.swift
+//  RandomDice
+//
+//  Created by 陳昕喬 on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct RandomDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
